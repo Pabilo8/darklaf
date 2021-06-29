b2dbb7b96a5c8c27dedea52d0ab22acaaa2a9074
weisj <31143295+weisJ@users.noreply.github.com>
2021-06-29 13:08:39 +0200

    Build: Add and compile module info
    We add a custom task to compile the module-info with a newer java version.
    To ensure the module-info is correct we need to compile it together with all other project sources.

--- windows/build.gradle.kts
+++ windows/build.gradle.kts
@@ -2,6 +2,7 @@
 
 plugins {
     java
+    `module-info-compile`
     id("dev.nokee.jni-library")
     id("dev.nokee.cpp-language")
     `uber-jni-jar`
@@ -22,23 +23,27 @@ library {
         resourcePath.set("com/github/weisj/darklaf/platform/${project.name}/${targetMachine.variantName}")
         sharedLibrary {
             compileTasks.configureEach {
-                compilerArgs.addAll(toolChain.map {
-                    when (it) {
-                        is Gcc, is Clang -> listOf("--std=c++17", "-Wall", "-Wextra", "-pedantic")
-                        is VisualCpp -> listOf("/std:c++17", "/EHsc", "/W4", "/permissive", "/WX")
-                        else -> emptyList()
+                compilerArgs.addAll(
+                    toolChain.map {
+                        when (it) {
+                            is Gcc, is Clang -> listOf("--std=c++17", "-Wall", "-Wextra", "-pedantic")
+                            is VisualCpp -> listOf("/std:c++17", "/EHsc", "/W4", "/permissive", "/WX")
+                            else -> emptyList()
+                        }
                     }
-                })
+                )
                 optimizedBinary()
             }
             linkTask.configure {
-                linkerArgs.addAll(toolChain.map {
-                    when (it) {
-                        is Gcc, is Clang -> listOf("-ldwmapi", "-lGdi32", "-luser32", "-ladvapi32", "-lShell32")
-                        is VisualCpp -> listOf("dwmapi.lib", "user32.lib", "Gdi32.lib", "Advapi32.lib", "Shell32.lib")
-                        else -> emptyList()
+                linkerArgs.addAll(
+                    toolChain.map {
+                        when (it) {
+                            is Gcc, is Clang -> listOf("-ldwmapi", "-lGdi32", "-luser32", "-ladvapi32", "-lShell32")
+                            is VisualCpp -> listOf("dwmapi.lib", "user32.lib", "Gdi32.lib", "Advapi32.lib", "Shell32.lib")
+                            else -> emptyList()
+                        }
                     }
-                })
+                )
             }
         }
     }
